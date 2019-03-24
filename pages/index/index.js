8532b784c3bd81e918e1ca5488d42bb829aa0a20
xcc <itxcc420@gmail.com>
2019-03-24 14:47:55 +0800

    简单版

---
+++ pages/index/index.js
@@ -0,0 +1,69 @@
+//index.js
+//获取应用实例
+const app = getApp();
+var chaiDate = require('../../utils/chai.js');
+Page({
+  data: {
+    inputValue: '在 止匕 车俞 入...',
+		chaiDate:''
+  },
+  onLoad: function () {
+		this.setData({chaiDate:chaiDate.chai});
+
+	},
+  //事件处理函数
+  bindViewTap: function() {
+    wx.navigateTo({
+      url: '../logs/logs'
+    })
+  },
+  bindKeyInput(e) {
+
+		var chaiDate = this.data.chaiDate;
+		var userValue = e.detail.value;
+		console.log(userValue);
+		this.setData({
+			inputValue: ''
+		});
+		if(userValue == ""){
+			this.setData({
+				inputValue: '在 止匕 车俞 入...'
+			});
+		}
+
+		//console.log(userValue);
+		//console.log(chaiDate);
+		for(var i=0;i<userValue.length;i++){
+			if(chaiDate.hasOwnProperty(userValue[i])){
+				this.setData({
+					inputValue: this.data.inputValue + chaiDate[userValue[i]]
+				});
+			}else{
+				this.setData({
+					inputValue:this.data.inputValue  +  userValue[i]
+				});
+			}
+		}
+
+
+	},
+  copyText: function (e) {
+		var txt = this.data.inputValue;
+		wx.setClipboardData({
+			data: txt,
+			success: function (res) {
+				wx.getClipboardData({
+					success: function (res) {
+						wx.showToast({
+							title: '内容已复制！',
+							icon: 'success',
+							duration: 1000
+						})
+					}
+				})
+			}
+		})
+	}
+
+
+})
